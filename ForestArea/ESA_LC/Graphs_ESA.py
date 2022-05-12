681c0a2d3ae2b6e87ce7ae3bedc2368cf0ce4fe2
Klara Janouskova <klarunkax@gmail.com>
2022-05-12 17:25:10 +0200

    x

--- ForestArea/ESA_LC/Graphs_ESA.py
+++ ForestArea/ESA_LC/Graphs_ESA.py
@@ -27,6 +27,8 @@ ESA_Forest = ESA_Forest.loc[:, ~ESA_Forest.columns.str.endswith('_40')]
 total_ESA_Forest = pandas.DataFrame(ESA_Forest)
 ESA_1992 = ESA_Forest.loc[:,ESA_Forest.columns.str.startswith('1992')]
 total_ESA_Forest['1992'] = ESA_1992.sum(axis=1)
+ESA_1995 = ESA_Forest.loc[:,ESA_Forest.columns.str.startswith('1995')]
+total_ESA_Forest['1995'] = ESA_1995.sum(axis=1)
 ESA_2000 = ESA_Forest.loc[:,ESA_Forest.columns.str.startswith('2000')]
 total_ESA_Forest['2000'] = ESA_2000.sum(axis=1)
 ESA_2005 = ESA_Forest.loc[:,ESA_Forest.columns.str.startswith('2005')]
@@ -44,11 +46,11 @@ total_ESA_Forest['2018'] = ESA_2018.sum(axis=1)
 ESA_2020 = ESA_Forest.loc[:,ESA_Forest.columns.str.startswith('2020')]
 total_ESA_Forest['2020'] = ESA_2020.sum(axis=1)
 #delete forest categories and keep only totals for each year
-total_ESA_Forest = total_ESA_Forest[['GRID_CODE', 'EEA39', 'EU27', '1992', '2000', '2005', '2006', '2010', '2012', '2015', '2018', '2020']]
+total_ESA_Forest = total_ESA_Forest[['GRID_CODE', 'EEA39', 'EU27', '1992', '1995', '2000', '2005', '2006', '2010', '2012', '2015', '2018', '2020']]
 
 
 #delete the country if it has NA for any of the years
-total_ESA_Forest_EEA39 = total_ESA_Forest.dropna(axis=0, how='any', subset=['1992', '2000', '2005', '2006', '2010', '2012', '2015', '2018', '2020'])
+total_ESA_Forest_EEA39 = total_ESA_Forest.dropna(axis=0, how='any', subset=['1992', '1995', '2000', '2005', '2006', '2010', '2012', '2015', '2018', '2020'])
 # A)create total forest for EEA39
 total_ESA_Forest_EEA39.loc['ESA_EEA39'] = total_ESA_Forest_EEA39.sum(numeric_only=True, axis=0)/1000000 #mil. hectares
 ESA_EEA39 = total_ESA_Forest_EEA39.loc["ESA_EEA39"]
@@ -56,7 +58,7 @@ ESA_EEA39 = ESA_EEA39.iloc[3:]
 #print(ESA_EEA39)
 
 # B.)create TOTAL FOREST for EU27
-total_ESA_Forest_EU27 = total_ESA_Forest.dropna(axis=0, how='any', subset=['1992', '2000', '2005', '2006', '2010', '2012', '2015', '2018', '2020'])
+total_ESA_Forest_EU27 = total_ESA_Forest.dropna(axis=0, how='any', subset=['1992', '1995', '2000', '2005', '2006', '2010', '2012', '2015', '2018', '2020'])
 total_ESA_Forest_EU27 = total_ESA_Forest_EU27[total_ESA_Forest_EU27["EU27"] == 1]
 print(total_ESA_Forest_EU27.to_string())
 total_ESA_Forest_EU27.loc['ESA_EU27'] = total_ESA_Forest_EU27.sum(numeric_only=True, axis=0)/1000000 #mil. hectares
@@ -73,6 +75,8 @@ core_ESA_Forest = core_ESA_Forest.loc[:, ~core_ESA_Forest.columns.str.endswith('
 ESA_1992 = core_ESA_Forest.loc[:,core_ESA_Forest.columns.str.startswith('1992')]
 total_core_ESA['1992'] = ESA_1992.sum(axis=1)
 #print(ESA_1992)
+ESA_1995 = core_ESA_Forest.loc[:,core_ESA_Forest.columns.str.startswith('1995')]
+total_core_ESA['1995'] = ESA_1995.sum(axis=1)
 ESA_2000 = core_ESA_Forest.loc[:,core_ESA_Forest.columns.str.startswith('2000')]
 total_core_ESA['2000'] = ESA_2000.sum(axis=1)
 #print(ESA_2000)
@@ -92,10 +96,10 @@ ESA_2020 = core_ESA_Forest.loc[:,core_ESA_Forest.columns.str.startswith('2020')]
 total_core_ESA['2020'] = ESA_2020.sum(axis=1)
 
 # #delete forest categories and keep only totals for each year
-total_core_ESA = total_core_ESA[['GRID_CODE', 'EEA39', 'EU27', '1992', '2000', '2005', '2006', '2010', '2012','2015', '2018', '2020']]
+total_core_ESA = total_core_ESA[['GRID_CODE', 'EEA39', 'EU27', '1992', '1995', '2000', '2005', '2006', '2010', '2012','2015', '2018', '2020']]
 #print(total_core_ESA.to_string())
 # # A)create CORE forest for EEA39
-total_core_ESA_EEA39 = total_core_ESA.dropna(axis=0, how='any', subset=['1992', '2000', '2005', '2006', '2010', '2012','2015', '2018', '2020'])
+total_core_ESA_EEA39 = total_core_ESA.dropna(axis=0, how='any', subset=['1992', '1995', '2000', '2005', '2006', '2010', '2012','2015', '2018', '2020'])
 total_core_ESA_EEA39.loc['ESA_core_EEA39'] = total_core_ESA_EEA39.sum(numeric_only=True, axis=0)/1000000 #mil. hectares
 #print(total_core_ESA_EEA39.to_string())
 
@@ -104,7 +108,7 @@ ESA_core_EEA39 = ESA_core_EEA39.iloc[3:]
 #print(ESA_core_EEA39)
 
 # B.)create CORE FOREST for EU27
-total_core_ESA_EU27 = total_core_ESA.dropna(axis=0, how='any', subset=['1992', '2000', '2005', '2006', '2010', '2012', '2015', '2018', '2020'])
+total_core_ESA_EU27 = total_core_ESA.dropna(axis=0, how='any', subset=['1992',  '1995', '2000', '2005', '2006', '2010', '2012', '2015', '2018', '2020'])
 total_core_ESA_EU27 = total_core_ESA_EU27[total_core_ESA_EU27["EU27"] == 1]
 total_core_ESA_EU27.loc['ESA_core_EU27'] = total_core_ESA_EU27.sum(numeric_only=True, axis=0)/1000000 #mil. hectares
 #print(total_core_ESA_EU27.to_string())
@@ -116,10 +120,12 @@ ESA_core_EU27 = ESA_core_EU27.iloc[3:]
 #print(ESA_Forest.to_string())
 
 total_NONcore_ESA = pandas.DataFrame(ESA_Forest)
-NONcore_ESA_Forest = ESA_Forest[['GRID_CODE', 'EEA39', 'EU27', '1992_100', '1992_110', '2000_100', '2000_110', '2005_100', '2005_110', '2006_100', '2006_110', '2010_100', '2010_110', '2012_100', '2012_110', '2015_100', '2015_110', '2018_100', '2018_110', '2020_100', '2020_110']]
+NONcore_ESA_Forest = ESA_Forest[['GRID_CODE', 'EEA39', 'EU27', '1992_100', '1992_110', '1995_100', '1995_110', '2000_100', '2000_110', '2005_100', '2005_110', '2006_100', '2006_110', '2010_100', '2010_110', '2012_100', '2012_110', '2015_100', '2015_110', '2018_100', '2018_110', '2020_100', '2020_110']]
 
 ESA_1992 = NONcore_ESA_Forest.loc[:,NONcore_ESA_Forest.columns.str.startswith('1992')]
 total_NONcore_ESA['1992'] = ESA_1992.sum(axis=1)
+ESA_1995 = NONcore_ESA_Forest.loc[:,NONcore_ESA_Forest.columns.str.startswith('1995')]
+total_NONcore_ESA['1995'] = ESA_1995.sum(axis=1)
 ESA_2000 = NONcore_ESA_Forest.loc[:,NONcore_ESA_Forest.columns.str.startswith('2000')]
 total_NONcore_ESA['2000'] = ESA_2000.sum(axis=1)
 ESA_2005 = NONcore_ESA_Forest.loc[:,NONcore_ESA_Forest.columns.str.startswith('2005')]
@@ -135,21 +141,21 @@ total_NONcore_ESA['2015'] = ESA_2015.sum(axis=1)
 ESA_2018 = NONcore_ESA_Forest.loc[:,NONcore_ESA_Forest.columns.str.startswith('2018')]
 total_NONcore_ESA['2018'] = ESA_2018.sum(axis=1)
 ESA_2020 = NONcore_ESA_Forest.loc[:,NONcore_ESA_Forest.columns.str.startswith('2020')]
-total_NONcore_ESA['2020'] = ESA_2018.sum(axis=1)
+total_NONcore_ESA['2020'] = ESA_2020.sum(axis=1)
 
 #delete forest categories and keep only totals for each year
-total_NONcore_ESA = total_NONcore_ESA[['GRID_CODE', 'EEA39', 'EU27', '1992', '2000', '2005', '2006', '2010', '2012','2015' , '2018', '2020']]
+total_NONcore_ESA = total_NONcore_ESA[['GRID_CODE', 'EEA39', 'EU27', '1992', '1995', '2000', '2005', '2006', '2010', '2012','2015' , '2018', '2020']]
 #print(total_NONcore_ESA.to_string())
 
 # A)create NON-CORE forest for EEA39
-total_NONcore_ESA_EEA39 = total_NONcore_ESA.dropna(axis=0, how='any', subset=['1992', '2000', '2005', '2006', '2010', '2012', '2015', '2018', '2020'])
+total_NONcore_ESA_EEA39 = total_NONcore_ESA.dropna(axis=0, how='any', subset=['1992', '1995','2000', '2005', '2006', '2010', '2012', '2015', '2018', '2020'])
 total_NONcore_ESA_EEA39.loc['ESA_NONcore_EEA39'] = total_NONcore_ESA_EEA39.sum(numeric_only=True, axis=0)/1000000 #mil. hectares
 ESA_NONcore_EEA39 = total_NONcore_ESA_EEA39.loc["ESA_NONcore_EEA39"]
 ESA_NONcore_EEA39 = ESA_NONcore_EEA39.iloc[3:]
 #print(ESA_NONcore_EEA39)
 
 # B.)create NON-CORE FOREST for EU27
-total_NONcore_ESA_EU27 = total_NONcore_ESA.dropna(axis=0, how='any', subset=['1992', '2000', '2005', '2006', '2010', '2012', '2018', '2020'])
+total_NONcore_ESA_EU27 = total_NONcore_ESA.dropna(axis=0, how='any', subset=['1992', '1995','2000', '2005', '2006', '2010', '2012', '2018', '2020'])
 total_NONcore_ESA_EU27 = total_NONcore_ESA_EU27[total_NONcore_ESA_EU27["EU27"] == 1]
 total_NONcore_ESA_EU27.loc['ESA_NONcore_EU27'] = total_NONcore_ESA_EU27.sum(numeric_only=True, axis=0)/1000000 #mil. hectares
 #print(total_core_ESA_EU27.to_string())
@@ -161,12 +167,13 @@ ESA_NONcore_EU27 = ESA_NONcore_EU27.iloc[3:]
 #A) ESA_EU27 Total CHANGE
 ESA_EU27 = pandas.DataFrame(ESA_EU27)
 ESA__change_EU27_t = ESA_EU27.T
-ESA__change_EU27_t["1992-2000"] = ESA__change_EU27_t["2000"] - ESA__change_EU27_t["1992"]
+ESA__change_EU27_t["1992-1995"] = ESA__change_EU27_t["1995"] - ESA__change_EU27_t["1992"]
+ESA__change_EU27_t["1995-2000"] = ESA__change_EU27_t["2000"] - ESA__change_EU27_t["1995"]
 ESA__change_EU27_t["2000-2005"] = ESA__change_EU27_t["2005"] - ESA__change_EU27_t["2000"]
 ESA__change_EU27_t["2005-2010"] = ESA__change_EU27_t["2010"] - ESA__change_EU27_t["2005"]
 ESA__change_EU27_t["2010-2015"] = ESA__change_EU27_t["2015"] - ESA__change_EU27_t["2010"]
 ESA__change_EU27_t["2015-2020"] = ESA__change_EU27_t["2020"] - ESA__change_EU27_t["2015"]
-ESA__change_EU27_t = ESA__change_EU27_t[['1992-2000', '2000-2005', '2005-2010', '2010-2015', '2015-2020']]
+ESA__change_EU27_t = ESA__change_EU27_t[['1992-1995', '1995-2000', '2000-2005', '2005-2010', '2010-2015', '2015-2020']]
 ESA_EU27_change = ESA__change_EU27_t.T
 # print(ESA_EU27_change)
 
@@ -197,12 +204,13 @@ ESA_EU27_change = ESA__change_EU27_t.T
 # D.) ESA_core_EU27 ChANGE
 ESA_core_EU27 = pandas.DataFrame(ESA_core_EU27)
 ESA__change_EU27_t = ESA_core_EU27.T
-ESA__change_EU27_t["1992-2000"] = ESA__change_EU27_t["2000"] - ESA__change_EU27_t["1992"]
+ESA__change_EU27_t["1992-1995"] = ESA__change_EU27_t["1995"] - ESA__change_EU27_t["1992"]
+ESA__change_EU27_t["1995-2000"] = ESA__change_EU27_t["2000"] - ESA__change_EU27_t["1995"]
 ESA__change_EU27_t["2000-2005"] = ESA__change_EU27_t["2005"] - ESA__change_EU27_t["2000"]
 ESA__change_EU27_t["2005-2010"] = ESA__change_EU27_t["2010"] - ESA__change_EU27_t["2005"]
 ESA__change_EU27_t["2010-2015"] = ESA__change_EU27_t["2015"] - ESA__change_EU27_t["2010"]
 ESA__change_EU27_t["2015-2020"] = ESA__change_EU27_t["2020"] - ESA__change_EU27_t["2015"]
-ESA__change_EU27_t = ESA__change_EU27_t[['1992-2000', '2000-2005', '2005-2010', '2010-2015', '2015-2020']]
+ESA__change_EU27_t = ESA__change_EU27_t[['1992-1995', '1995-2000', '2000-2005', '2005-2010', '2010-2015', '2015-2020']]
 ESA_core_EU27_change = ESA__change_EU27_t.T
 
 # print(ESA_core_EU27_change)
@@ -222,12 +230,13 @@ ESA_core_EU27_change = ESA__change_EU27_t.T
 # F.) ESA_NONcore_EU27 CHANGE
 ESA_NONcore_EU27 = pandas.DataFrame(ESA_NONcore_EU27)
 ESA__change_EU27_t = ESA_NONcore_EU27.T
-ESA__change_EU27_t["1992-2000"] = ESA__change_EU27_t["2000"] - ESA__change_EU27_t["1992"]
+ESA__change_EU27_t["1992-1995"] = ESA__change_EU27_t["1995"] - ESA__change_EU27_t["1992"]
+ESA__change_EU27_t["1995-2000"] = ESA__change_EU27_t["2000"] - ESA__change_EU27_t["1995"]
 ESA__change_EU27_t["2000-2005"] = ESA__change_EU27_t["2005"] - ESA__change_EU27_t["2000"]
 ESA__change_EU27_t["2005-2010"] = ESA__change_EU27_t["2010"] - ESA__change_EU27_t["2005"]
 ESA__change_EU27_t["2010-2015"] = ESA__change_EU27_t["2015"] - ESA__change_EU27_t["2010"]
 ESA__change_EU27_t["2015-2020"] = ESA__change_EU27_t["2020"] - ESA__change_EU27_t["2015"]
-ESA__change_EU27_t = ESA__change_EU27_t[['1992-2000', '2000-2005', '2005-2010', '2010-2015', '2015-2020']]
+ESA__change_EU27_t = ESA__change_EU27_t[['1992-1995', '1995-2000', '2000-2005', '2005-2010', '2010-2015', '2015-2020']]
 ESA_NONcore_EU27_change = ESA__change_EU27_t.T
 # print(ESA_NONcore_EU27_change)
 #
--- ForestArea/UNECE/Graphs_UNECE.py
+++ ForestArea/UNECE/Graphs_UNECE.py
@@ -12,140 +12,138 @@ UNECE_Forest = pandas.read_csv('UNECE_forestArea v2.csv')
 UNECE_Forest = pandas.DataFrame(UNECE_Forest)
 #delete Poland and Hungary where we miss OWL
 UNECE_Forest.rename(columns={ UNECE_Forest.columns[0]: "country" }, inplace = True)
-UNECE_Forest = UNECE_Forest[UNECE_Forest['country'] != 'Hungary']
-UNECE_Forest = UNECE_Forest[UNECE_Forest['country'] != 'Poland']
 UNECE_Forest = UNECE_Forest[UNECE_Forest['country'] != 'Malta']
 UNECE_Forest = UNECE_Forest[UNECE_Forest["EU"] == 1]
 print(UNECE_Forest.to_string())
 
 #core forest
-UNECE_core_EU25 = UNECE_Forest.iloc[:, 2:8]
-UNECE_core_EU25 = UNECE_core_EU25.rename(columns={"Forest - 2020 ( 1000 ha )": "2020", "Forest - 2015 ( 1000 ha )": "2015", "Forest - 2010 ( 1000 ha )": "2010", "Forest - 2005 ( 1000 ha )": "2005", "Forest - 2000 ( 1000 ha )": "2000", "Forest - 1990 ( 1000 ha )": "1990"})
-print(UNECE_core_EU25.to_string())
-UNECE_core_EU25.loc['UNECE_core_EU25'] = UNECE_core_EU25.sum(numeric_only=True, axis=0)/1000 #mil. hectares
-UNECE_core_EU25 = UNECE_core_EU25.loc["UNECE_core_EU25"]
-UNECE_core_EU25 = UNECE_core_EU25.sort_index()
-print(UNECE_core_EU25)
+UNECE_EU27_Forest = UNECE_Forest.iloc[:, 2:8]
+UNECE_EU27_Forest = UNECE_EU27_Forest.rename(columns={"Forest - 2020 ( 1000 ha )": "2020", "Forest - 2015 ( 1000 ha )": "2015", "Forest - 2010 ( 1000 ha )": "2010", "Forest - 2005 ( 1000 ha )": "2005", "Forest - 2000 ( 1000 ha )": "2000", "Forest - 1990 ( 1000 ha )": "1990"})
+print(UNECE_EU27_Forest.to_string())
+UNECE_EU27_Forest.loc['UNECE_EU27_Forest'] = UNECE_EU27_Forest.sum(numeric_only=True, axis=0)/1000 #mil. hectares
+UNECE_EU27_Forest = UNECE_EU27_Forest.loc["UNECE_EU27_Forest"]
+UNECE_EU27_Forest = UNECE_EU27_Forest.sort_index()
+print(UNECE_EU27_Forest)
 
 #TOTAL FOREST
 
-UNECE_EU25 = UNECE_Forest.iloc[:, 14:20]
-UNECE_EU25 = UNECE_EU25.rename(columns={"Total forest and OWL - 2020 ( 1000 ha )": "2020", "Total forest and OWL - 2015 ( 1000 ha )": "2015", "Total forest and OWL - 2010 ( 1000 ha )": "2010", "Total forest and OWL - 2005 ( 1000 ha )": "2005", "Total forest and OWL - 2000 ( 1000 ha )": "2000", "Total forest and OWL - 1990 ( 1000 ha )": "1990"})
-print(UNECE_EU25.to_string())
-UNECE_EU25.loc['UNECE_EU25'] = UNECE_EU25.sum(numeric_only=True, axis=0)/1000 #mil. hectares
-UNECE_EU25 = UNECE_EU25.loc["UNECE_EU25"]
-UNECE_EU25 = UNECE_EU25.sort_index()
-print(UNECE_EU25)
+UNECE_EU27_ForestAndOWL = UNECE_Forest.iloc[:, 14:20]
+UNECE_EU27_ForestAndOWL = UNECE_EU27_ForestAndOWL.rename(columns={"Total forest and OWL - 2020 ( 1000 ha )": "2020", "Total forest and OWL - 2015 ( 1000 ha )": "2015", "Total forest and OWL - 2010 ( 1000 ha )": "2010", "Total forest and OWL - 2005 ( 1000 ha )": "2005", "Total forest and OWL - 2000 ( 1000 ha )": "2000", "Total forest and OWL - 1990 ( 1000 ha )": "1990"})
+print(UNECE_EU27_ForestAndOWL.to_string())
+UNECE_EU27_ForestAndOWL.loc['UNECE_EU27_ForestAndOWL'] = UNECE_EU27_ForestAndOWL.sum(numeric_only=True, axis=0)/1000 #mil. hectares
+UNECE_EU27_ForestAndOWL = UNECE_EU27_ForestAndOWL.loc["UNECE_EU27_ForestAndOWL"]
+UNECE_EU27_ForestAndOWL = UNECE_EU27_ForestAndOWL.sort_index()
+print(UNECE_EU27_ForestAndOWL)
 
 #NON CORE FOREST
 
-UNECE_NONcore_EU25 = UNECE_Forest.iloc[:, 8:14]
-UNECE_NONcore_EU25 = UNECE_NONcore_EU25.rename(columns={"OWL - 2020 ( 1000 ha )": "2020", "OWL - 2015 ( 1000 ha )": "2015", "OWL - 2010 ( 1000 ha )": "2010", "OWL - 2005 ( 1000 ha )": "2005", "OWL - 2000 ( 1000 ha )": "2000", "OWL - 1990 ( 1000 ha )": "1990"})
-print(UNECE_NONcore_EU25.to_string())
-UNECE_NONcore_EU25.loc['UNECE_NONcore_EU25'] = UNECE_NONcore_EU25.sum(numeric_only=True, axis=0)/1000 #mil. hectares
-UNECE_NONcore_EU25 = UNECE_NONcore_EU25.loc["UNECE_NONcore_EU25"]
-UNECE_NONcore_EU25 = UNECE_NONcore_EU25.sort_index()
-print(UNECE_NONcore_EU25)
+UNECE_EU27_OWL = UNECE_Forest.iloc[:, 8:14]
+UNECE_EU27_OWL = UNECE_EU27_OWL.rename(columns={"OWL - 2020 ( 1000 ha )": "2020", "OWL - 2015 ( 1000 ha )": "2015", "OWL - 2010 ( 1000 ha )": "2010", "OWL - 2005 ( 1000 ha )": "2005", "OWL - 2000 ( 1000 ha )": "2000", "OWL - 1990 ( 1000 ha )": "1990"})
+print(UNECE_EU27_OWL.to_string())
+UNECE_EU27_OWL.loc['UNECE_EU27_OWL'] = UNECE_EU27_OWL.sum(numeric_only=True, axis=0)/1000 #mil. hectares
+UNECE_EU27_OWL = UNECE_EU27_OWL.loc["UNECE_EU27_OWL"]
+UNECE_EU27_OWL = UNECE_EU27_OWL.sort_index()
+print(UNECE_EU27_OWL)
 
 #
 # # 4.) Change data
-# #A) UNECE_EU25 Total CHANGE
-UNECE_EU25 = pandas.DataFrame(UNECE_EU25)
-UNECE_change_EU25_t = UNECE_EU25.T
-UNECE_change_EU25_t["1990-2000"] = UNECE_change_EU25_t["2000"] - UNECE_change_EU25_t["1990"]
-UNECE_change_EU25_t["2000-2005"] = UNECE_change_EU25_t["2005"] - UNECE_change_EU25_t["2000"]
-UNECE_change_EU25_t["2005-2010"] = UNECE_change_EU25_t["2010"] - UNECE_change_EU25_t["2005"]
-UNECE_change_EU25_t["2010-2015"] = UNECE_change_EU25_t["2015"] - UNECE_change_EU25_t["2010"]
-UNECE_change_EU25_t["2015-2020"] = UNECE_change_EU25_t["2020"] - UNECE_change_EU25_t["2015"]
-UNECE_change_EU25_t = UNECE_change_EU25_t[['1990-2000', '2000-2005', '2005-2010', '2010-2015', '2015-2020']]
-UNECE_EU25_change = UNECE_change_EU25_t.T
-print(UNECE_EU25_change)
+# #A) UNECE_EU27_ForestAndOWL Total CHANGE
+UNECE_EU27_ForestAndOWL = pandas.DataFrame(UNECE_EU27_ForestAndOWL)
+UNECE_change_EU27_t = UNECE_EU27_ForestAndOWL.T
+UNECE_change_EU27_t["1990-2000"] = UNECE_change_EU27_t["2000"] - UNECE_change_EU27_t["1990"]
+UNECE_change_EU27_t["2000-2005"] = UNECE_change_EU27_t["2005"] - UNECE_change_EU27_t["2000"]
+UNECE_change_EU27_t["2005-2010"] = UNECE_change_EU27_t["2010"] - UNECE_change_EU27_t["2005"]
+UNECE_change_EU27_t["2010-2015"] = UNECE_change_EU27_t["2015"] - UNECE_change_EU27_t["2010"]
+UNECE_change_EU27_t["2015-2020"] = UNECE_change_EU27_t["2020"] - UNECE_change_EU27_t["2015"]
+UNECE_change_EU27_t = UNECE_change_EU27_t[['1990-2000', '2000-2005', '2005-2010', '2010-2015', '2015-2020']]
+UNECE_EU27_ForestAndOWL_change = UNECE_change_EU27_t.T
+print(UNECE_EU27_ForestAndOWL_change)
 
 #
-# # D.) UNECE_core_EU25 ChANGE
-UNECE_core_EU25 = pandas.DataFrame(UNECE_core_EU25)
-UNECE_change_EU25_t = UNECE_core_EU25.T
-UNECE_change_EU25_t["1990-2000"] = UNECE_change_EU25_t["2000"] - UNECE_change_EU25_t["1990"]
-UNECE_change_EU25_t["2000-2005"] = UNECE_change_EU25_t["2005"] - UNECE_change_EU25_t["2000"]
-UNECE_change_EU25_t["2005-2010"] = UNECE_change_EU25_t["2010"] - UNECE_change_EU25_t["2005"]
-UNECE_change_EU25_t["2010-2015"] = UNECE_change_EU25_t["2015"] - UNECE_change_EU25_t["2010"]
-UNECE_change_EU25_t["2015-2020"] = UNECE_change_EU25_t["2020"] - UNECE_change_EU25_t["2015"]
-UNECE_change_EU25_t = UNECE_change_EU25_t[['1990-2000', '2000-2005', '2005-2010', '2010-2015', '2015-2020']]
-UNECE_core_EU25_change = UNECE_change_EU25_t.T
-print(UNECE_core_EU25_change)
+# # D.) UNECE_EU27_Forest ChANGE
+UNECE_EU27_Forest = pandas.DataFrame(UNECE_EU27_Forest)
+UNECE_change_EU27_t = UNECE_EU27_Forest.T
+UNECE_change_EU27_t["1990-2000"] = UNECE_change_EU27_t["2000"] - UNECE_change_EU27_t["1990"]
+UNECE_change_EU27_t["2000-2005"] = UNECE_change_EU27_t["2005"] - UNECE_change_EU27_t["2000"]
+UNECE_change_EU27_t["2005-2010"] = UNECE_change_EU27_t["2010"] - UNECE_change_EU27_t["2005"]
+UNECE_change_EU27_t["2010-2015"] = UNECE_change_EU27_t["2015"] - UNECE_change_EU27_t["2010"]
+UNECE_change_EU27_t["2015-2020"] = UNECE_change_EU27_t["2020"] - UNECE_change_EU27_t["2015"]
+UNECE_change_EU27_t = UNECE_change_EU27_t[['1990-2000', '2000-2005', '2005-2010', '2010-2015', '2015-2020']]
+UNECE_EU27_Forest_change = UNECE_change_EU27_t.T
+print(UNECE_EU27_Forest_change)
 #
 #
 #
-# # F.) UNECE_NONcore_EU25 CHANGE
-UNECE_NONcore_EU25 = pandas.DataFrame(UNECE_NONcore_EU25)
-UNECE_change_EU25_t = UNECE_NONcore_EU25.T
-UNECE_change_EU25_t["1990-2000"] = UNECE_change_EU25_t["2000"] - UNECE_change_EU25_t["1990"]
-UNECE_change_EU25_t["2000-2005"] = UNECE_change_EU25_t["2005"] - UNECE_change_EU25_t["2000"]
-UNECE_change_EU25_t["2005-2010"] = UNECE_change_EU25_t["2010"] - UNECE_change_EU25_t["2005"]
-UNECE_change_EU25_t["2010-2015"] = UNECE_change_EU25_t["2015"] - UNECE_change_EU25_t["2010"]
-UNECE_change_EU25_t["2015-2020"] = UNECE_change_EU25_t["2020"] - UNECE_change_EU25_t["2015"]
-UNECE_change_EU25_t = UNECE_change_EU25_t[['1990-2000', '2000-2005', '2005-2010', '2010-2015', '2015-2020']]
-UNECE_NONcore_EU25_change = UNECE_change_EU25_t.T
-print(UNECE_NONcore_EU25_change)
+# # F.) UNECE_EU27_OWL CHANGE
+UNECE_EU27_OWL = pandas.DataFrame(UNECE_EU27_OWL)
+UNECE_change_EU27_t = UNECE_EU27_OWL.T
+UNECE_change_EU27_t["1990-2000"] = UNECE_change_EU27_t["2000"] - UNECE_change_EU27_t["1990"]
+UNECE_change_EU27_t["2000-2005"] = UNECE_change_EU27_t["2005"] - UNECE_change_EU27_t["2000"]
+UNECE_change_EU27_t["2005-2010"] = UNECE_change_EU27_t["2010"] - UNECE_change_EU27_t["2005"]
+UNECE_change_EU27_t["2010-2015"] = UNECE_change_EU27_t["2015"] - UNECE_change_EU27_t["2010"]
+UNECE_change_EU27_t["2015-2020"] = UNECE_change_EU27_t["2020"] - UNECE_change_EU27_t["2015"]
+UNECE_change_EU27_t = UNECE_change_EU27_t[['1990-2000', '2000-2005', '2005-2010', '2010-2015', '2015-2020']]
+UNECE_EU27_OWL_change = UNECE_change_EU27_t.T
+print(UNECE_EU27_OWL_change)
 # #
 # # 5.) ready data
 #
-sum_UNECE_EU25 = pandas.concat([UNECE_EU25, UNECE_core_EU25, UNECE_NONcore_EU25], axis=1, join="inner")
-sum_UNECE_EU25.to_csv('C:/Users/Klara/Documents/Prace/JRC/Teleworking/2022/Forest area EU/Data/sum_tables/sum_UNECE_EU25.csv')
-print(sum_UNECE_EU25)
+sum_UNECE_EU27_ForestAndOWL = pandas.concat([UNECE_EU27_ForestAndOWL, UNECE_EU27_Forest, UNECE_EU27_OWL], axis=1, join="inner")
+sum_UNECE_EU27_ForestAndOWL.to_csv('C:/Users/Klara/Documents/Prace/JRC/Teleworking/2022/Forest area EU/Data/sum_tables/sum_UNECE_EU27.csv')
+print(sum_UNECE_EU27_ForestAndOWL)
 #
-change_UNECE_EU25 = pandas.concat([UNECE_EU25_change, UNECE_core_EU25_change, UNECE_NONcore_EU25_change], axis=1, join="inner")
-change_UNECE_EU25.to_csv('C:/Users/Klara/Documents/Prace/JRC/Teleworking/2022/Forest area EU/Data/sum_tables/change_UNECE_EU25.csv')
-# #print(change_UNECE_EU25)7_change = UNECE_NONcore_EU25_change.reset_index(level=0)
+change_UNECE_EU27_ForestAndOWL = pandas.concat([UNECE_EU27_ForestAndOWL_change, UNECE_EU27_Forest_change, UNECE_EU27_OWL_change], axis=1, join="inner")
+change_UNECE_EU27_ForestAndOWL.to_csv('C:/Users/Klara/Documents/Prace/JRC/Teleworking/2022/Forest area EU/Data/sum_tables/change_UNECE_EU27.csv')
+# #print(change_UNECE_EU27_ForestAndOWL)7_change = UNECE_EU27_OWL_change.reset_index(level=0)
 
 #
 #
 # # #
 # # # 6.) plot charts
 #
-UNECE_EU25.plot.bar(
-ylabel="milion hectares", xlabel="year", title="UNECE Forest+Other wooded land in EU25", legend=None)
-plt.ylim(bottom=0, top=180)
-plt.savefig('C:/Users/Klara/Documents/Prace/JRC/Teleworking/2022/Forest area EU/Data/output_charts/UNECE_EU25.png')
+UNECE_EU27_ForestAndOWL.plot.bar(
+ylabel="milion hectares", xlabel="year", title="UNECE Forest+Other wooded land in EU27", legend=None)
+plt.ylim(bottom=0, top=200)
+plt.savefig('C:/Users/Klara/Documents/Prace/JRC/Teleworking/2022/Forest area EU/Data/output_charts/UNECE_EU27_ForestAndOWL.png')
 plt.show()
 #
 
-UNECE_core_EU25.plot.bar(
-ylabel="milion hectares", xlabel="year", title="UNECE Forest in EU25", legend=None)
-plt.ylim(bottom=0, top=180)
-plt.savefig('C:/Users/Klara/Documents/Prace/JRC/Teleworking/2022/Forest area EU/Data/output_charts/UNECE_core_EU25.png')
+UNECE_EU27_Forest.plot.bar(
+ylabel="milion hectares", xlabel="year", title="UNECE Forest in EU27", legend=None)
+plt.ylim(bottom=0, top=200)
+plt.savefig('C:/Users/Klara/Documents/Prace/JRC/Teleworking/2022/Forest area EU/Data/output_charts/UNECE_EU27_Forest.png')
 plt.show()
 # #
 
-UNECE_NONcore_EU25.plot.bar(
-ylabel="milion hectares", xlabel="years", title="Other wooded land in EU25", legend=None)
-plt.ylim(bottom=0, top=180)
-plt.savefig('C:/Users/Klara/Documents/Prace/JRC/Teleworking/2022/Forest area EU/Data/output_charts/UNECE_NONcore_EU25.png')
+UNECE_EU27_OWL.plot.bar(
+ylabel="milion hectares", xlabel="years", title="Other wooded land in EU27", legend=None)
+plt.ylim(bottom=0, top=200)
+plt.savefig('C:/Users/Klara/Documents/Prace/JRC/Teleworking/2022/Forest area EU/Data/output_charts/UNECE_EU27_OWL.png')
 plt.show()
 # #
 # # #
 
 
-UNECE_EU25_change.plot.bar(
-ylabel="milion hectares", xlabel="Years", title="UNECE change in Forest + Other wooded land in EU25", legend=None)
+UNECE_EU27_ForestAndOWL_change.plot.bar(
+ylabel="milion hectares", xlabel="Years", title="UNECE change in Forest + Other wooded land in EU27", legend=None)
 plt.ylim(bottom=-4, top=7)
 plt.xticks(rotation='horizontal')
 plt.axhline(y=0, color='r', linestyle='-')
-plt.savefig('C:/Users/Klara/Documents/Prace/JRC/Teleworking/2022/Forest area EU/Data/output_charts/UNECE_EU25_change.png')
+plt.savefig('C:/Users/Klara/Documents/Prace/JRC/Teleworking/2022/Forest area EU/Data/output_charts/UNECE_EU27_ForestAndOWL_change.png')
 plt.show()
 
-UNECE_core_EU25_change.plot.bar(
-ylabel="milion hectares", xlabel="years", title="UNECE change in Forest in EU25", legend=None)
+UNECE_EU27_Forest_change.plot.bar(
+ylabel="milion hectares", xlabel="years", title="UNECE change in Forest in EU27", legend=None)
 plt.ylim(bottom=-4, top=7)
 plt.xticks(rotation='horizontal')
 plt.axhline(y=0, color='r', linestyle='-')
-plt.savefig('C:/Users/Klara/Documents/Prace/JRC/Teleworking/2022/Forest area EU/Data/output_charts/UNECE_core_EU25_change.png')
+plt.savefig('C:/Users/Klara/Documents/Prace/JRC/Teleworking/2022/Forest area EU/Data/output_charts/UNECE_EU27_Forest_change.png')
 plt.show()
 
-UNECE_NONcore_EU25_change.plot.bar(
-ylabel="milion hectares", xlabel="years", title="UNECE change in Other wooded land in EU25", legend=None)
+UNECE_EU27_OWL_change.plot.bar(
+ylabel="milion hectares", xlabel="years", title="UNECE change in Other wooded land in EU27", legend=None)
 plt.ylim(bottom=-4, top=7)
 plt.xticks(rotation='horizontal')
 plt.axhline(y=0, color='r', linestyle='-')
-plt.savefig('C:/Users/Klara/Documents/Prace/JRC/Teleworking/2022/Forest area EU/Data/output_charts/UNECE_NONcore_EU25_change.png')
+plt.savefig('C:/Users/Klara/Documents/Prace/JRC/Teleworking/2022/Forest area EU/Data/output_charts/UNECE_EU27_OWL_change.png')
 plt.show()
